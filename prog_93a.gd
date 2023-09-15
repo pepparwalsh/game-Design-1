74cedcd615d0161550011e640a5dc8e57c2ae836
pepparwalsh <143834261+pepparwalsh@users.noreply.github.com>
2023-09-15 12:47:49 -0500

    Update prog_93a.gd
    I understand everything i just have some trouble remembering all the codes

--- prog_93a.gd
+++ prog_93a.gd
@@ -9,18 +9,29 @@ func _ready():
 func _process(delta):
 	pass
 
-
+	
+	
+func _on_clear_pressed():
+	$Line1.text = ""
+	
+	
 func _on_exit_pressed():
 	get_tree().quit()
 	
-
-
-func _on_clear_pressed():$Line1.text = ""
+	
 
 
 func _on_calculate_pressed():
-	var 
-	var 
-	var 
+	var watts = int($Line1.text)
+	var Baserate = watts * 0.0475
+	var Surcharge = Baserate * .10
+	var Citytax = Baserate * .03
+	var Total = Baserate + Surcharge + Citytax
+	var LatePayment = (Total * 0.04) + Total 
+	$Label2.text = "Baserate="+str(Baserate)+"\nSurcharge="+str(Surcharge)+"\nCitytax="+str(Citytax)+"\nLatePayment="+str(LatePayment)+"\nTotal="+str(Total)
+	
+	
+	
+	
 	
 	
