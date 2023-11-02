a78e99006a4e8d02428d1d17add8196709a8aa3a
pepparwalsh <143834261+pepparwalsh@users.noreply.github.com>
2023-11-02 12:47:32 -0500

    idk how to make objects solid

--- phys_demo.gd
+++ phys_demo.gd
@@ -13,7 +13,7 @@ func _process(delta):
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
 		var nball = preload("res://phys_ball.tscn").instantiate()
-		nball.posistion = event.position
+		nball.position = event.position
 		add_child(nball)
 		
 func _on_gravslider_value_changed(value):
@@ -21,13 +21,19 @@ func _on_gravslider_value_changed(value):
 		if child is RigidBody2D:
 			child.apply_central_impulse(Vector2(1,0)*500)
 			
-	func _on_button_2-pressed():
-		var V_inertia = float($Panel/txtInertia.text)
-		var V_X = float($Panel/txtVx.text)
-		var V_Y = float($Panel/txtVy.text)
-		var velcoiy = Vector2(V_X, V_Y)
-		for child in get_children():
-			if child is RigidBody2D:
-				child.inertia = V_inertia
-				child.Linear_velocity = velocity
-		
+func _on_button_2_pressed():
+			var V_inertia = float($Panel/txtinertia.text)
+			var V_X = int($Panel/txtvx.text)
+			var V_Y = int($Panel/txtvy.text)
+			var velocity = Vector2(V_X, V_Y)
+			for child in get_children():
+				if child is RigidBody2D:
+					child.inertia= V_inertia
+					child.linear_velocity = velocity
+
+
+func _on_button_pressed():
+	for child in get_children():
+		if child is RigidBody2D:
+			child.apply_central_impulse(Vector2.DOWN * 500)
+			
