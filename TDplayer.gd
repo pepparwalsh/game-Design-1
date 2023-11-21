ef3406f431cb4913ab1a439c953ca12ff7620829
pepparwalsh <143834261+pepparwalsh@users.noreply.github.com>
2023-11-21 12:51:20 -0600

    happy birthdayyyy!!!:)

--- TDplayer.gd
+++ TDplayer.gd
@@ -5,22 +5,47 @@ const SPEED = 100.0  # 80.0
 # const JUMP_VELOCITY = -400.0
 const MAX_OBTAINABLE_HEALTH = 400.0
 
+enum STATES { IDLE=0, DEAD, DAMAGED, ATTACKING, CHANGING }
+
 @export var data = {
 	"max_health": 60.0, # 20hp per heart; 5 per fraction
 	"health": 60.0,
 	"money": 0,
+	"state": STATES.IDLE,
+	"secondaries": [],
 }
 var inertia = Vector2()
 var look_direction = Vector2.DOWN  # (0, 1)
+var attack_direction = Vector2.DOWN
+var animation_lock = 0.0 # locl player while playing attack animation
 
+var slash_scene = preload("res://entities/attacks/slash.tscn")
 var menu_scene = preload("res://my_gui.tscn")
 var menu_instance = null
 
 @onready var p_HUD = get_tree().get_first_node_in_group("HUD")
 
+func get_direction_name():
+	return ["right", "down", "left", "up"][
+		int(round(look_direction.angle() * 2 / PI)) % 4
+	]
+	
+func attack():
+	data.state = STATES.ATTACKING
+	$AnimatedSprite2D.play("swipe_" + get_direction_name())
+	attack_direction = look_direction
+	var slash = slash_scene.instantiate()
+	slash.position = attack_direction * 20.0
+	slash.rotation = Vector2().angle_to_point(-attack_dirction)
+	add_child(slash)
+	animation_lock = 0.2
+	
 func pickup_money(value):
 	data.money += value
 	
+func pickup_heart(value):
+	data.health+= value
+	data.health = clamp(data.health, 0, data.max_health)
 
 func _ready():
 	p_HUD.show()
@@ -45,6 +70,11 @@ func _physics_process(delta):
 	velocity += inertia
 	move_and_slide()
 	inertia = inertia.move_toward(Vector2(), delta * 1000.0)
+	
+	if data.state != STATES.DEAD:
+		if Input.is_action_just_pressed("ui_accept"):
+			attack()
+	
 	
 	if Input.is_action_just_pressed("ui_cancel"):
 		menu_instance.show()
---
+++ entities/attacks/slash.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+
+var damage = 10
+var knockback = 64.0
+var anim_life = 0.2
+
+
+func _ready():
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
